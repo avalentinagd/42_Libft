88a5d2408809929becc6ece99835ba93d4da397c
avalentinagd <avalentinagd@gmail.com>
2026-06-30 17:54:30 +0200

    memcmp & strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,69 +6,67 @@
 /*   By: angalleg <angalleg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/25 22:15:37 by angalleg          #+#    #+#             */
-/*   Updated: 2026/06/26 00:28:15 by angalleg         ###   ########.fr       */
+/*   Updated: 2026/06/30 17:24:13 by angalleg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
-#include <string.h>
 
-char    *ft_strnstr(const char *big, const char *little, size_t len)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-    size_t i;
-    size_t j;
-    
-    i = 0;
-    if (!*little)
-        return ((char *)big);
-    while (big[i] != '\0' && i < len)
-    {
-        j = 0;
-        while ((i + j < len) && little[j] == big[i + j] && little[j] != '\0')
-            j++;
-        if (little[j] == '\0')
-            return ((char *)&big[i]);
-        i++;
-    }
-    return (NULL);
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (!*little)
+		return ((char *)big);
+	while (big[i] != '\0' && i < len)
+	{
+		j = 0;
+		while ((i + j < len) && little[j] == big[i + j] && little[j] != '\0')
+			j++;
+		if (little[j] == '\0')
+			return ((char *)&big[i]);
+		i++;
+	}
+	return (NULL);
 }
 
 #include <stdio.h>
-//#include <bsd/string.h>
+#include <bsd/string.h>
 int main(void)
 {
-    char *big =    "aaabcabcd";
-    
+    char *big =    "aaabcabcd"; 
     // --- TEST 1: Caso normal para encontrar "cd" ---
     printf("--- TEST 1 (len = 9 - Encuentra) ---\n");
-    printf("Tu ft    : >%s<\n\n", ft_strnstr(big, "cd", 9));
-    //printf("Original : >%s<\n", strnstr(big, "cd", 9));
+    printf("Tu ft    : >%s<\n", ft_strnstr(big, "cd", 9));
+    printf("Original : >%s<\n\n", strnstr(big, "cd", 9));
 
     // --- TEST 2: Limite que corta ---
     printf("--- TEST 2 (len = 8 - Corto) ---\n");
-    printf("Tu ft    : >%s<\n\n", ft_strnstr(big, "cd", 8));
-    //printf("Original : >%s<\n", strnstr(big, "cd", 8));
+    printf("Tu ft    : >%s<\n", ft_strnstr(big, "cd", 8));
+    printf("Original : >%s<\n\n", strnstr(big, "cd", 8));
 
     // --- TEST 3: Aguja vacia ---
     printf("--- TEST 3 (Aguja vacia) ---\n");
-    printf("Tu ft    : >%s<\n\n", ft_strnstr(big, "", 5));
-    //printf("Original : >%s<\n", strnstr(big, "", 5));
+    printf("Tu ft    : >%s<\n", ft_strnstr(big, "", 5));
+    printf("Original : >%s<\n\n", strnstr(big, "", 5));
 
     // --- TEST 4: Coincidencia parcial ---
     // Buscamos "abcde" donde solo existe "abcd"
     printf("--- TEST 4 (Coincidencia parcial) ---\n");
-    printf("Tu ft    : >%s<\n\n", ft_strnstr(big, "abcde", 9));
-    //printf("Original : >%s<\n", strnstr(big, "abcde", 9));
+    printf("Tu ft    : >%s<\n", ft_strnstr(big, "abcde", 9));
+    printf("Original : >%s<\n\n", strnstr(big, "abcde", 9));
 
     // --- TEST 5: len = 0 ---
     printf("--- TEST 5 (len = 0) ---\n");
-    printf("Tu ft    : >%s<\n\n", ft_strnstr(big, "aa", 0));
-    //printf("Original : >%s<\n", strnstr(big, "aa", 0));
+    printf("Tu ft    : >%s<\n", ft_strnstr(big, "aa", 0));
+    printf("Original : >%s<\n\n", strnstr(big, "aa", 0));
 
     // --- TEST 6: len = 0 ---
     printf("--- TEST 6 (len = 0 y little = \'\\0') ---\n");
-    printf("Tu ft    : >%s<\n\n", ft_strnstr(big, "", 0));
-    //printf("Original : >%s<\n", strnstr(big, "aa", 0));
+    printf("Tu ft    : >%s<\n", ft_strnstr(big, "", 0));
+    printf("Original : >%s<\n\n", strnstr(big, "", 0));
 
     return (0);
 }
